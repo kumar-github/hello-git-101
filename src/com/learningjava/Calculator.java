f82c81d07638215cbf1cd96edab9546f57c5a721
Saravana Kumar M <kumar9885035454@gmail.com>
2025-02-01 20:22:06 +0530

    Refactored the add method.

--- src/com/learningjava/Calculator.java
+++ src/com/learningjava/Calculator.java
@@ -9,8 +9,8 @@ public class Calculator {
     public boolean isOdd(int number) {
         return number % 2 == 1;
     }
-    public int addingTwoNumbers(int number1, int number2)
-    {
+
+    public int add(int number1, int number2) {
         return number1 + number2;
     }
 }

529d48d39d4eac8cef46f01390b89780f0f7874f
Saravana Kumar M <kumar9885035454@gmail.com>
2025-02-01 13:37:08 +0530

    Commit-1

---
+++ src/com/learningjava/App.java
@@ -0,0 +1,13 @@
+package com.learningjava;
+
+public class App {
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.go();
+    }
+
+    private void go() {
+        // call the new code from here
+    }
+}
